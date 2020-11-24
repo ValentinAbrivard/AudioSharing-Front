72de2442f1b412451168056492837b3f9db9b045
ValentinAbrivard <39702869+ValentinAbrivard@users.noreply.github.com>
2020-11-24 15:51:31 +0100

    VueJS index + room display + install

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+import Room from './Room.vue'
+var test = 1
+
+createApp(App).mount('#app')
+createApp(Room).mount('#room')
